649c2b5c801846f2cc101e70d6469e29a05fcd6d
fworg64 <fworg64@gmail.com>
2018-08-24 22:58:36 -0600

    updated header file with comments

--- src/mapping/PC2CM/include/pc2_processor/pc2_processor.h
+++ src/mapping/PC2CM/include/pc2_processor/pc2_processor.h
@@ -5,13 +5,27 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 
+//struct for input parameters like
+//grid_width, min number of elements for a grid cell
+
+
+//
+
 class pc2cmProcessor
 {
 public:
-     pc2cmProcessor(int one);
+     pc2cmProcessor(double grid_width, double grid_min_elements); //struct
      bool getOne();
+     //add points to grid
+     //calculate new costmap
+     //->median filter on grid?
+     //->do DoG on height map
+     //->project height map onto global costmap
 private:
     bool isOne;
+    //array containing average heights projected onto ground plane estimate
+      //ground plane shoud be known from tf of camera to base_link 
+        //(might get revised with time and data)
 }; 
 
 #endif
