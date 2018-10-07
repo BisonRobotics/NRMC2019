997d7540d770ab72fd36d925748035cbc92c8cf7
jacobhuesman <jacob.huesman@ndsu.edu>
2018-10-06 23:33:05 -0700

    Fixed header guards

--- src/controls/teleop/include/qt_joystick/main_window.h
+++ src/controls/teleop/include/qt_joystick/main_window.h
@@ -1,5 +1,5 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
 
 #include <QMainWindow>
 #include <QTimer>
@@ -42,4 +42,4 @@ class MainWindow : public QMainWindow
     uint32_t seq;
 };
 
-#endif // MAINWINDOW_H
+#endif // MAIN_WINDOW_H
