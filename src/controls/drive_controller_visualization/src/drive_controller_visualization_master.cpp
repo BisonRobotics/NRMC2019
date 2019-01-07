a4d8969b2c4db60720ebf42e9b71d44da84e3850
fworg64 <fworg64@gmail.com>
2019-01-07 11:00:36 -0700

    added shell for veloctiy plot, also fixed segfault on exit

--- src/controls/drive_controller_visualization/src/drive_controller_visualization_master.cpp
+++ src/controls/drive_controller_visualization/src/drive_controller_visualization_master.cpp
@@ -12,18 +12,30 @@
 
 #include <tf/transform_listener.h>
 
-#define NUM_SERIES_PLOT_1 6
-#define Y_MIN_POS_PLOT -5.0
-#define Y_MAX_POS_PLOT 5.0
-#define NUM_Y_TICKS 5
-#define PLOT_TIME_WINDOW 30.0
-#define PLOT_TIME_TICK_PERIOD 5.0
+#define NUM_SERIES_PLOT1 6
+#define Y_MIN_POS_PLOT1 -5.0
+#define Y_MAX_POS_PLOT1 5.0
+#define NUM_Y_TICKS_PLOT1 5
+#define TIME_WINDOW_PLOT1 30.0
+#define TIME_TICK_PERIOD_PLOT1 5.0
 
-cv::String names[] = {"Xpos", "Ypos", "Angle", "Xest", "Yest", "Angleest"};
+#define NUM_SERIES_PLOT2 6
+#define Y_MIN_POS_PLOT2 -5.0
+#define Y_MAX_POS_PLOT2 5.0
+#define NUM_Y_TICKS_PLOT2 5
+#define TIME_WINDOW_PLOT2 30.0
+#define TIME_TICK_PERIOD_PLOT2 5.0
+
+cv::String names[] = {"Xpos", "Ypos", "Theta", "Xest", "Yest", "Thetaest"};
+cv::String names2[] = {"Xvel", "Yvel", "Omega", "Xvest", "Yvest", "Omegaest"};
 cv::Scalar colors[] = {cv::Scalar(60,180,120), cv::Scalar(240,180,60), cv::Scalar(120, 60, 180),
                        cv::Scalar(60, 60,120), cv::Scalar(120,180,60), cv::Scalar(120, 180,180)};
-dcvis_multiplot dcvmp(NUM_SERIES_PLOT_1, "Positions", Y_MIN_POS_PLOT, Y_MAX_POS_PLOT, NUM_Y_TICKS,
-                      PLOT_TIME_WINDOW, PLOT_TIME_TICK_PERIOD, names, colors);
+                       
+dcvis_multiplot dcvmp(NUM_SERIES_PLOT1, "Positions", Y_MIN_POS_PLOT1, Y_MAX_POS_PLOT1, NUM_Y_TICKS_PLOT1,
+                      TIME_WINDOW_PLOT1, TIME_TICK_PERIOD_PLOT1, names, colors);
+                      
+dcvis_multiplot dcvmv(NUM_SERIES_PLOT2, "Positions", Y_MIN_POS_PLOT2, Y_MAX_POS_PLOT2, NUM_Y_TICKS_PLOT2,
+                      TIME_WINDOW_PLOT2, TIME_TICK_PERIOD_PLOT2, names2, colors);
 ros::Time start_time;
 
 tf::TransformListener* listener = NULL;
@@ -83,22 +95,22 @@ int main(int argc, char** argv)
     static const int plotsize_width  = 400;
     static const int plotsize_height = 330;
     cv::Mat plotarea1(plotsize_height,plotsize_width,CV_8UC3, cv::Scalar(0,0,0));
-    
-
+    cv::Mat plotarea2(plotsize_height,plotsize_width,CV_8UC3, cv::Scalar(0,0,0));
     
     dcvmp.draw(plotarea1);
+    dcvmv.draw(plotarea2);
         
-    cv::Mat framebuff(400, 800, CV_8UC3, cv::Scalar(170,70,70));
+    cv::Mat framebuff(400, 950, CV_8UC3, cv::Scalar(170,70,70));
+    //First you draw a circle...
     cv::Point ppp(2,2);
     cv::circle(framebuff, ppp, 5, cv::Scalar(200,200,200));
     
-    plotarea1.copyTo(framebuff(cv::Rect(50,35,plotsize_width,plotsize_height)));
+    plotarea1.copyTo(framebuff(cv::Rect(50 ,35,plotsize_width,plotsize_height)));
+    plotarea2.copyTo(framebuff(cv::Rect(500,35,plotsize_width, plotsize_height)));
     
     cv::namedWindow("disp", cv::WINDOW_AUTOSIZE);
     cv::imshow("disp", framebuff);
-    
-    //ros::Rate r(30);
-    
+        
     std::cout << "Press q (in image window) to quit\n";
     int key_code = 0;
     int idex = 0;
@@ -110,23 +122,22 @@ int main(int argc, char** argv)
         last_time = curr_time;
         curr_time = ros::Time::now();
         loop_time = curr_time - last_time;
-        //std::cout << "LT: " <<loop_time.toSec() <<'\n';
-        //dcvmp.add_point(std::sin(5.0*((double)idex++)/200.0 - 5.0), 0);
+
         dcvmp.draw(plotarea1);
+        dcvmv.draw(plotarea2);
         
-        plotarea1.copyTo(framebuff(cv::Rect(50,35,plotsize_width,plotsize_height)));
+        plotarea1.copyTo(framebuff(cv::Rect(50 ,35,plotsize_width,plotsize_height)));
+        plotarea2.copyTo(framebuff(cv::Rect(500,35,plotsize_width,plotsize_height)));
         cv::imshow("disp", framebuff);
+        
+        ros::spinOnce();        
 
         key_code = cv::waitKey(30); //needed to service UI thread
         if (key_code == 113 || key_code == 81)
         {
             std::cout << "quit registered" << '\n';
+            delete listener;
             return 0;
         }
-        //r.sleep(); wait is handled by waitkey
-        ros::spinOnce();
-        
     }
-    delete listener;
-    
 }
