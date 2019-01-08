9e5fe7fd079062fdf61f4de80474b488559f9a34
fworg64 <fworg64@gmail.com>
2019-01-07 18:42:25 -0700

    added imu to graphs, next will be state vector published

--- src/controls/drive_controller_visualization/src/drive_controller_visualization_master.cpp
+++ src/controls/drive_controller_visualization/src/drive_controller_visualization_master.cpp
@@ -12,6 +12,8 @@
 
 #include <tf/transform_listener.h>
 
+#include <vrep_msgs/IMU.h>
+
 #define NUM_SERIES_PLOT1 6
 #define Y_MIN_POS_PLOT1 -5.0
 #define Y_MAX_POS_PLOT1 5.0
@@ -26,16 +28,30 @@
 #define TIME_WINDOW_PLOT2 30.0
 #define TIME_TICK_PERIOD_PLOT2 5.0
 
+#define NUM_SERIES_PLOT3 2
+#define Y_MIN_POS_PLOT3 -.5
+#define Y_MAX_POS_PLOT3 .5
+#define NUM_Y_TICKS_PLOT3 5
+#define TIME_WINDOW_PLOT3 30.0
+#define TIME_TICK_PERIOD_PLOT3 5.0
+
 cv::String names[] = {"Xpos", "Ypos", "Theta", "Xest", "Yest", "Thetaest"};
 cv::String names2[] = {"Xvel", "Yvel", "Omega", "Xvest", "Yvest", "Omegaest"};
-cv::Scalar colors[] = {cv::Scalar(60,180,120), cv::Scalar(240,180,60), cv::Scalar(120, 60, 180),
+cv::String names3[] = {"Xacc", "Yacc", "Xaest", "Yaest"};
+cv::Scalar colors6[] = {cv::Scalar(60,180,120), cv::Scalar(240,180,60), cv::Scalar(120, 60, 180),
                        cv::Scalar(60, 60,120), cv::Scalar(120,180,60), cv::Scalar(120, 180,180)};
+cv::Scalar colors4[] = {cv::Scalar(60,180,120), cv::Scalar(240,180,60),
+                       cv::Scalar(60, 60,120), cv::Scalar(120,180,60)};
                        
 dcvis_multiplot dcvmp(NUM_SERIES_PLOT1, "Positions", Y_MIN_POS_PLOT1, Y_MAX_POS_PLOT1, NUM_Y_TICKS_PLOT1,
-                      TIME_WINDOW_PLOT1, TIME_TICK_PERIOD_PLOT1, names, colors);
+                      TIME_WINDOW_PLOT1, TIME_TICK_PERIOD_PLOT1, names, colors6);
                       
-dcvis_multiplot dcvmv(NUM_SERIES_PLOT2, "Positions", Y_MIN_POS_PLOT2, Y_MAX_POS_PLOT2, NUM_Y_TICKS_PLOT2,
-                      TIME_WINDOW_PLOT2, TIME_TICK_PERIOD_PLOT2, names2, colors);
+dcvis_multiplot dcvmv(NUM_SERIES_PLOT2, "Velocities", Y_MIN_POS_PLOT2, Y_MAX_POS_PLOT2, NUM_Y_TICKS_PLOT2,
+                      TIME_WINDOW_PLOT2, TIME_TICK_PERIOD_PLOT2, names2, colors6);
+                      
+dcvis_multiplot dcvma(NUM_SERIES_PLOT3, "Accelerations", Y_MIN_POS_PLOT3, Y_MAX_POS_PLOT3, NUM_Y_TICKS_PLOT3,
+                      TIME_WINDOW_PLOT3, TIME_TICK_PERIOD_PLOT3, names3, colors4);
+                      
 ros::Time start_time;
 
 tf::TransformListener* listener = NULL;
@@ -84,6 +100,14 @@ void poseCallback(const geometry_msgs::PoseStamped::ConstPtr &msg)
     dcvmp.add_point(theta , t, 5);
 }
 
+void accelerationCallback(const vrep_msgs::IMU::ConstPtr &msg) //is this right?
+{
+    ros::Duration plot_time = msg->header.stamp - start_time;
+    double t = plot_time.toSec();
+    dcvma.add_point(msg->linear_acceleration.x, t, 0);
+    dcvma.add_point(msg->linear_acceleration.y, t, 1);
+}
+
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "drive_controller_vis");
@@ -91,22 +115,26 @@ int main(int argc, char** argv)
     start_time = ros::Time::now();
     listener = new tf::TransformListener();
     ros::Subscriber posSub = n.subscribe("/vrep/pose", 100, poseCallback);
+    ros::Subscriber imuSub = n.subscribe("/vrep/imu", 100, accelerationCallback);
 
     static const int plotsize_width  = 400;
     static const int plotsize_height = 330;
     cv::Mat plotarea1(plotsize_height,plotsize_width,CV_8UC3, cv::Scalar(0,0,0));
     cv::Mat plotarea2(plotsize_height,plotsize_width,CV_8UC3, cv::Scalar(0,0,0));
+    cv::Mat plotarea3(plotsize_height,plotsize_width,CV_8UC3, cv::Scalar(0,0,0));
     
     dcvmp.draw(plotarea1);
     dcvmv.draw(plotarea2);
+    dcvmv.draw(plotarea3);
         
-    cv::Mat framebuff(400, 950, CV_8UC3, cv::Scalar(170,70,70));
+    cv::Mat framebuff(950, 950, CV_8UC3, cv::Scalar(170,70,70));
     //First you draw a circle...
     cv::Point ppp(2,2);
     cv::circle(framebuff, ppp, 5, cv::Scalar(200,200,200));
     
     plotarea1.copyTo(framebuff(cv::Rect(50 ,35,plotsize_width,plotsize_height)));
     plotarea2.copyTo(framebuff(cv::Rect(500,35,plotsize_width, plotsize_height)));
+    plotarea3.copyTo(framebuff(cv::Rect(500,330 + 2*35,plotsize_width, plotsize_height)));
     
     cv::namedWindow("disp", cv::WINDOW_AUTOSIZE);
     cv::imshow("disp", framebuff);
@@ -125,9 +153,11 @@ int main(int argc, char** argv)
 
         dcvmp.draw(plotarea1);
         dcvmv.draw(plotarea2);
+        dcvma.draw(plotarea3);
         
         plotarea1.copyTo(framebuff(cv::Rect(50 ,35,plotsize_width,plotsize_height)));
         plotarea2.copyTo(framebuff(cv::Rect(500,35,plotsize_width,plotsize_height)));
+        plotarea3.copyTo(framebuff(cv::Rect(500,330 + 2*35,plotsize_width,plotsize_height)));
         cv::imshow("disp", framebuff);
         
         ros::spinOnce();        
