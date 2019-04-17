99bf984ddd56403869b55dd095863905da8bf414
fworg64 <fworg64@gmail.com>
2019-04-17 13:51:18 -0600

    fixed drive controller tests and added speed limit

--- src/hardware_layer/vesc_access/include/vesc_access/mock_vesc_access.h
+++ src/hardware_layer/vesc_access/include/vesc_access/mock_vesc_access.h
@@ -15,7 +15,7 @@ public:
   MOCK_METHOD0(getPotPosition, float(void));
   MOCK_METHOD1(setDuty, void(float));
   MOCK_METHOD1(setCustom, void(float));
-  MOCK_METHOD0(getADC(), int(void));
+  MOCK_METHOD0(getADC, int(void));
 };
 
 #endif
