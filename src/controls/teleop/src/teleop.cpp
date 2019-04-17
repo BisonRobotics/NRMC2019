f622251813fb1dc39e0ce5ba84c686a4328eed65
jacobhuesman <jacob.huesman@ndsu.edu>
2019-04-17 12:36:00 -0500

    Teleop bug fix

--- src/controls/teleop/src/teleop.cpp
+++ src/controls/teleop/src/teleop.cpp
@@ -178,17 +178,18 @@ int main(int argc, char **argv)
     }
     if (drive_safety)
     {
+      //ROS_INFO("| L | %f | R | %f |", left_wheels, right_wheels);
       teleop.update(left_wheels, right_wheels);
     }
     else
     {
       teleop.update(0.0f, 0.0f);
     }
-    ROS_INFO("| P | %5i | DS | %15s | CD | %20s | BH | %9s |",
+    /*ROS_INFO("| P | %5i | DS | %15s | CD | %20s | BH | %9s |",
         dig_controller.getCentralDrivePosition(),
         dig_controller.getDigStateString().c_str(),
         dig_controller.getCentralDriveStateString().c_str(),
-        dig_controller.getBackhoeStateString().c_str());
+        dig_controller.getBackhoeStateString().c_str());*/
     rate.sleep();
   }
 }
