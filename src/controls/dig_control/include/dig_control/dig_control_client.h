fcb7557be9a82c89d246dee0a963fbcd7d213372
jacobhuesman <jacob.huesman@ndsu.edu>
2019-04-19 13:48:37 -0500

    Started on dig_control_client

--- src/controls/dig_control/include/dig_control/dig_control_client.h
+++ src/controls/dig_control/include/dig_control/dig_control_client.h
@@ -1,9 +1,38 @@
 #ifndef DIG_CONTROL_DIG_CONTROL_CLIENT_H
 #define DIG_CONTROL_DIG_CONTROL_CLIENT_H
 
+#include <ros/ros.h>
+#include <dig_control/DigControlAction.h>
+#include <dig_control/dig_controller.h>
+#include <actionlib/client/simple_action_client.h>
+
 namespace dig_control
 {
+  class DigControlClient
+  {
+  public:
+    DigControlClient();
 
+    void setControlState(ControlState state);
+
+    void doneCallback(const actionlib::SimpleClientGoalState &state, const DigControlResultConstPtr &result);
+    void activeCallback();
+    void feedbackCallback(const DigControlFeedbackConstPtr &feedback);
+
+    ControlState getControlState() const;
+    DigState getDigState() const;
+    CentralDriveState getCentralDriveState() const;
+    BackhoeState getBackhoeState() const;
+    BucketState getBucketState() const;
+
+  private:
+    actionlib::SimpleActionClient<dig_control::DigControlAction> client;
+    ControlState control_state;
+    DigState dig_state;
+    CentralDriveState central_drive_state;
+    BackhoeState backhoe_state;
+    BucketState bucket_state;
+  };
 }
 
 #endif //DIG_CONTROL_DIG_CONTROL_CLIENT_H
