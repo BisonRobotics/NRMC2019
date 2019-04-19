670c9bdc0a8c5721e7e19ce635523ee5829ba7d9
jacobhuesman <jacob.huesman@ndsu.edu>
2019-04-19 10:24:58 -0500

    Moved dig_control teleop code over to dig_controller_server

--- src/controls/dig_control/include/dig_control/dig_control_server.h
+++ src/controls/dig_control/include/dig_control/dig_control_server.h
@@ -5,6 +5,7 @@
 #include <actionlib/server/simple_action_server.h>
 #include <dig_control/DigControlAction.h>
 #include <dig_control/dig_controller.h>
+#include <sensor_msgs/Joy.h>
 
 namespace dig_control
 {
@@ -14,13 +15,19 @@ namespace dig_control
     explicit DigControlServer(ros::NodeHandle *nh, DigController *controller);
 
     void goalCallback(const actionlib::SimpleActionServer<DigControlAction>::GoalConstPtr &goal);
+    void joyCallback(const sensor_msgs::Joy::ConstPtr &joy);
     void preemptCallback();
+    void update();
 
     static DigControlResult toResult(ControlState state);
     static ControlState toControlState(DigControlGoal goal);
 
   private:
+    bool dig_safety;
+    float backhoe_duty, bucket_duty, central_duty, vibrator_duty;
+
     ros::NodeHandle *nh;
+    ros::Subscriber joy_subscriber;
     actionlib::SimpleActionServer<dig_control::DigControlAction> server;
     bool active_request;
     DigController *controller;
--- src/controls/dig_control/src/dig_control_server.cpp
+++ src/controls/dig_control/src/dig_control_server.cpp
@@ -5,9 +5,12 @@ using namespace dig_control;
 
 
 DigControlServer::DigControlServer(ros::NodeHandle *nh, DigController *controller) :
+  dig_safety(false), backhoe_duty(0.0f), bucket_duty(0.0f), central_duty(0.0f), vibrator_duty(0.0f),
   nh(nh), controller(controller),
   server(*nh, "dig_control", boost::bind(&DigControlServer::goalCallback, this, _1), false)
 {
+  joy_subscriber = nh->subscribe("joy", 1, &DigControlServer::joyCallback, this);
+  controller->setControlState(ControlState::manual);
   //server.registerPreemptCallback(boost::bind(&DigControlServer::preemptCallback, this));
   server.start();
 }
@@ -94,6 +97,101 @@ void DigControlServer::goalCallback(const actionlib::SimpleActionServer<DigContr
   }
 }
 
+void DigControlServer::joyCallback(const sensor_msgs::Joy::ConstPtr &joy)
+{
+  bool dig_safety;
+
+  bool x  = joy->buttons[0] == 1; // Bucket down
+  bool a  = joy->buttons[1] == 1; // Linear actuator in
+  bool b  = joy->buttons[2] == 1; // Linear actuator out
+  bool y  = joy->buttons[3] == 1; // Bucket up
+  bool lb = joy->buttons[4] == 1; // Drive safety
+  bool rb = joy->buttons[5] == 1; // Dig safety
+  bool lt = joy->buttons[6] == 1; // Vibrator off
+  bool rt = joy->buttons[7] == 1; // Vibrator on
+  bool st = joy->buttons[9] == 1; // Start automatic dig
+  float ls = joy->axes[1];        // Left wheels
+  float rs = joy->axes[3];        // Right wheels
+  bool up = joy->axes[5] >  0.5;  // Central drive up (Up on left pad)
+  bool dp = joy->axes[5] < -0.5;  // Central drive down (Down on left pad)
+
+  dig_safety = rb;
+
+  if (dig_safety)
+  {
+    // Update bucket (Maintain state)
+    if (x && y)
+    {
+      ROS_WARN("[teleop] Conflicting commands, x and y are both pressed, stopping motion");
+      bucket_duty = 0.0;
+    }
+    else if (x)
+    {
+      bucket_duty = -BucketDuty::fast;
+    }
+    else if (y)
+    {
+      bucket_duty = BucketDuty::fast;
+    }
+
+    // Update backhoe (Maintain state)
+    if (a && b)
+    {
+      ROS_WARN("[teleop] Conflicting commands, a and b are both pressed, stopping motion");
+      backhoe_duty = 0.0f;
+    }
+    else if (a)
+    {
+      backhoe_duty = -BackhoeDuty::normal;
+    }
+    else if (b)
+    {
+      backhoe_duty = BackhoeDuty::fast;
+    }
+
+    // Update central drive
+    if (up && dp)
+    {
+      ROS_WARN("[teleop] Conflicting commands, up and dp are both pressed, stopping motion");
+      central_duty = 0.0f;
+    }
+    else if (up)
+    {
+      central_duty = CentralDriveDuty::fast;
+    }
+    else if (dp)
+    {
+      central_duty = -CentralDriveDuty::normal;
+    }
+    else
+    {
+      central_duty = 0.0f;
+    }
+
+    // Update vibrator (Maintain state)
+    if (lt && rt)
+    {
+      ROS_WARN("[teleop] Conflicting commands, lt and rt are both pressed, stopping motion");
+      vibrator_duty = 0.0f;
+    }
+    else if (lt)
+    {
+      vibrator_duty = 0.0f;
+    }
+    else if (rt)
+    {
+      vibrator_duty = 0.75;
+    }
+  }
+  else
+  {
+    central_duty = 0.0f;
+    backhoe_duty = 0.0f;
+    bucket_duty = 0.0f;
+    vibrator_duty = 0.0f;
+  }
+}
+
 void DigControlServer::preemptCallback()
 {
   ROS_INFO("[digControlPreemptCallback]");
@@ -111,6 +209,31 @@ ControlState DigControlServer::toControlState(DigControlGoal goal)
   return (ControlState)goal.control_state;
 }
 
+void DigControlServer::update()
+{
+  controller->update();
+  ControlState dig_state = controller->getControlState();
+  if (dig_safety)
+  {
+    if (dig_state == ControlState::error)
+    {
+      ROS_ERROR("Dig controller is in an error state");
+      controller->stop();
+    }
+    else if (dig_state == ControlState::manual)
+    {
+      controller->setCentralDriveDuty(central_duty);
+      controller->setBackhoeDuty(backhoe_duty);
+      controller->setVibratorDuty(vibrator_duty);
+      controller->setBucketDuty(bucket_duty);
+    }
+  }
+  else
+  {
+    controller->setControlState(ControlState::manual);
+    controller->stop();
+  }
+}
 
 int main(int argc, char* argv[])
 {
@@ -121,7 +244,7 @@ int main(int argc, char* argv[])
   ros::Rate rate(50);
   while (ros::ok())
   {
-    controller.update();
+    server.update();
     ros::spinOnce();
     rate.sleep();
   }
